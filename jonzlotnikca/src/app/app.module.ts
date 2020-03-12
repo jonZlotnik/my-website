a264cdedd0d2be3c53835f88447f9fc1f73080fa
Jon Zlotnik <j.zlotnik@live.ca>
2020-03-12 00:14:44 -0400

    initialized all landing page components and added old website to the repo

--- jonzlotnikca/src/app/app.module.ts
+++ jonzlotnikca/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SubHeaderComponent } from './sub-header/sub-header.component';
+import { ProfilePictureComponent } from './profile-picture/profile-picture.component';
+import { NavComponent } from './nav/nav.component';
+import { TitleComponent } from './title/title.component';
+import { SubTitleComponent } from './sub-title/sub-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SubHeaderComponent,
+    ProfilePictureComponent,
+    NavComponent,
+    TitleComponent,
+    SubTitleComponent
   ],
   imports: [
     BrowserModule,
---
+++ old-website/public_html/iosDev/MembersServer.js
@@ -0,0 +1,57 @@
+angular
+    .module("Members", [])
+    .service("memberServer", function() {
+
+//var firebase = require('firebase');
+//
+//function GetUsers() {
+//    if(navigator.onLine){
+//        GetUsersInternal();
+//    }
+//    else {
+//        console.log("No internet connection detected");
+//        return null;
+//    }
+//}
+//
+//function GetUsersInternal() {
+//    
+// var idRef = firebase.database().ref(DatabaseNode+"/Users");
+// idRef.on('value', snapshot => {  
+//     var users = [];
+//     snapshot.forEach(user => {
+//         var userObj = {
+//             Usernamae: user.child("Username").val(),
+//             First_Name: user.child("First Name").val(),
+//             Last_Name: user.child("Last Name").val(),
+//             Birthday: user.child("Birthday").val(),
+//             BrotherName: user.child("BrotherName").val(),
+//             Contribution: user.child("None").val(),
+//             Degree: user.child("Degree").val(),
+//             Email: user.child("Degree").val(),
+//             Grad: user.child("GraduationDate").val(),
+//             ImageURL: user.child("Image").val(),
+//             NotificationId: user.child("NotificationId").val(),
+//             Position: user.child("Position").val(),
+//             School: user.child("School").val(),
+//             UserId: user.child("UserID").val
+//         };
+//         users.push(userObj);
+//     });
+//     
+//     return users;
+// }).catch(error => {
+//     console.log("Failed to make firebase call");
+//     return null;
+// });
+//        
+//    
+// }
+});
+    
+
+
+
+
+
+
