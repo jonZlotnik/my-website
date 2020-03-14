92aeac2d5ca1e9e4527297370d9a5889c63e8ffe
Jon Zlotnik <j.zlotnik@live.ca>
2020-03-14 15:49:59 -0400

    adding little bit of routing and changed font sizes

--- jonzlotnikca/src/app/app-routing.module.ts
+++ jonzlotnikca/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ResumePageComponent } from './resume-page/resume-page.component';
+import {LandingPageComponent} from './landing-page/landing-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'resume', component: ResumePageComponent},
+  {path: '', component: LandingPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
