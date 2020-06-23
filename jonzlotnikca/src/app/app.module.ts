b7559b56282ad3abf65f42c07539f2ff23216a23
Jon Zlotnik <j.zlotnik@live.ca>
2020-06-23 19:54:50 -0400

    finished website skeleton playing around with image galleries

--- jonzlotnikca/src/app/app.module.ts
+++ jonzlotnikca/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,6 +25,7 @@ import { PhotographyPageComponent } from './photography-page/photography-page.co
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
