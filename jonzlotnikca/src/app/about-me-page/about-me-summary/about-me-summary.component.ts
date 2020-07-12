ddad6ba2ef3a7140bd0c6a55e8b8bb0647dffcb2
Jon Zlotnik <j.zlotnik@live.ca>
2020-07-12 13:46:01 -0400

    added blurbs to about me page

--- jonzlotnikca/src/app/about-me-page/about-me-summary/about-me-summary.component.ts
+++ jonzlotnikca/src/app/about-me-page/about-me-summary/about-me-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-about-me-summary',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me-summary.component.scss']
 })
 export class AboutMeSummaryComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() paragraphs: string;
 
   constructor() { }
 
