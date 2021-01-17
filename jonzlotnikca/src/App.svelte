e20b0acd4007fdad81cdf4b9bbd2e508b5d5ca1b
Jon Zlotnik <j.zlotnik@live.ca>
2021-01-17 11:29:05 -0500

    live reload support

--- jonzlotnikca/src/App.svelte
+++ jonzlotnikca/src/App.svelte
@@ -1,26 +1,30 @@
 <script>
-	export let name;
 </script>
 
+<header>
+	<h1>Jon Zlotnik</h1>
+</header>
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h2>About me</h2>
 </main>
 
 <style>
+	header {
+		width: 100%;
+		border-bottom: 1px solid white;
+		text-align: center;
+		padding-top: 1em;
+	}
+	header h1 {
+		font-size: 3em;
+		padding: 0;
+	}
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+	
 
 	@media (min-width: 640px) {
 		main {
